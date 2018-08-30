40689e7a6cfd1ceeb4c676c91d49f82773ce97fe
Mike <mshah.475@gmail.com>
2018-08-30 15:08:02 -0400

    Add files via upload

---
+++ Assignment2_CDataStructures/main.c
@@ -0,0 +1,37 @@
+// Include parts of the C Standard Library
+// These have been written by some other really
+// smart engineers.
+#include <stdio.h>  // For IO operations
+#include <stdlib.h> // for malloc/free
+
+// Our library that we have written.
+// Also, by a really smart engineer!
+#include "myqueue.h"
+// Note that we are locating this file
+// within the same directory, so we use quotations.
+
+
+// A sample test of your program
+// You can add as many unit tests as you like
+// We will be adding our own to test your program.
+void unitTest1(){
+
+	queue_t* test1 = create_queue(1);
+	printf("Attempting to add %d\n",1);
+	queue_enqueue(test1,1);	
+	printf("Removing: %d\n",queue_dequeue(test1));	
+
+	free_queue(test1);
+}
+
+
+// ====================================================
+// ================== Program Entry ===================
+// ====================================================
+int main(){
+
+	// List of Unit Tests to test your data structure	
+	unitTest1();
+
+	return 0;
+}
