d5f2d2727d0bbb43850c5ef73141ab59e2e153f0
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-09-16 13:46:47 -0400

    upload main.c

--- Lab1_UnixTerminal/TerminalOverview/main.c
+++ Lab1_UnixTerminal/TerminalOverview/main.c
@@ -1,1 +1,38 @@
-// Add your program here!
+//John Nguyen
+//C program power function
+
+#include <stdio.h>
+
+int power(int,int);
+
+int main(){
+	printf("Hello World");
+	int i;
+	for(i=0;i<10;i++){
+		printf("\n");
+		printf("%d", power(2,i));
+	}
+	return 0;
+}
+
+int power(int base,int n){
+	int result = base;
+
+	if(n==0){
+	result = 1;
+	}
+
+	else if (n==1){
+	result = base;
+	}
+
+	else{
+	int i;
+	for(i =1; i<=n; i++){
+	result *= base;
+	}
+	}
+
+	return result;
+}
+
