2bd817dde425e80b072ba78f9586b97d00c4b508
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-01 14:12:32 -0400

    Upload Lab3

--- Lab3_Assembly/for.c
+++ Lab3_Assembly/for.c
@@ -1,1 +1,18 @@
 // Write a C program using a for-loop that counts to 5.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+	int i;
+	for(i = 0; i<=5; i++)
+		printf("Loop number %d\n ", i);
+
+	return 0;
+
+}
+
+
