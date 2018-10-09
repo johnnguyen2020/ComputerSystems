3f7b858ea3691abb1848698de73bbe3d1b85702c
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-08 22:33:22 -0400

    added comments

--- Lab4_Linkers/trace/trace.c
+++ Lab4_Linkers/trace/trace.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
+//counter to keep track of function call count 
 int counter = 0;
+
+//variables to calculate function elapsed time
 time_t start_t;
 time_t end_t;
 double diff_t;
+
 
 __attribute__((no_instrument_function))
 void __cyg_profile_func_enter(void *this_fn, void *call_site){
 
 	printf("Function entered\n");
-	counter+=1;
-	time(&start_t);
-	sleep(5);
+	counter+=1; //increment counter by 1 every time function is called
+	time(&start_t); //start timer
+	sleep(5); //pause program by 5 seconds to show timer funcitonality
 
 }
 
@@ -20,10 +24,10 @@ __attribute__((no_instrument_function))
 void __cyg_profile_func_exit(void *this_fn, void *call_site){
 
 	printf("Function exited\n");
-	printf("%d\n",counter);
-	time(&end_t);
-	diff_t = difftime(end_t, start_t);
-	printf("Execution time = %f\n", diff_t);
+	printf("%d\n",counter); //print count
+	time(&end_t); //end timer
+	diff_t = difftime(end_t, start_t); //calc end time minus start time 
+	printf("Execution time = %f\n", diff_t); //print function elapse time
 
 }
 
