3a2f6b06655bcaae765922b38c4d9f0b9212c88f
johnnguyen2020 <32847409+johnnguyen2020@users.noreply.github.com>
2018-10-14 12:02:15 -0400

    Update shell.c

--- Assignment4_Shell/shell.c
+++ Assignment4_Shell/shell.c
@@ -1,6 +1,27 @@
 // Modify this file for your assignment
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h> // This is new!
+
+
+// Create a signal handler
+void sigint_handler(int sig){
+	// Ask yourself why 35?
+	write(1,"Terminating through signal handler\n",35); 
+	exit(0);
+}
 
 int main(){
 
-  return 0;
+	// Install our signal handler
+	signal(SIGINT, sigint_handler);
+
+	printf("You can only terminate by pressing Ctrl+C\n");
+
+	while(1){
+		printf("Running forever!\n");
+		sleep(1);
+	}
+
+	return 0;
 }
