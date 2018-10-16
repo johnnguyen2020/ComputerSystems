a212601d3d3cc97e4ad41f194e2586159710d998
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-15 22:57:13 -0400

    upload 4

--- Assignment4_Shell/shell.c
+++ Assignment4_Shell/shell.c
@@ -3,27 +3,159 @@
 #include <stdlib.h>
 #include <signal.h> // This is new!
 #include <unistd.h> 
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+const int MAX_BUF_SZ = 80;
+char str_exit[4];
 
 // Create a signal handler
 void sigint_handler(int sig){
 	// Ask yourself why 35?
-	write(1,"Terminating through signal handler\n",35); 
+	write(1,"mini-shell terminatd\n",21); 
 	exit(0);
 }
 
-int main(){
 
+void parse(char *line, char **argv){
+	while (*line != '\0'){
+		while (*line == ' ' || *line == '\t' || *line == '\n')
+			*line++ = '\0';
+			*argv++ = line;
+		while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+			line++;
+	}
+	*argv = '\0';
+}
+
+void execute(char **argv){
+	pid_t pid;
+	int status;
+	if((pid = fork()) < 0){
+		printf("Command not found\n");	
+		exit(1);
+	}
+	
+	else if(pid == 0){
+		if (execvp(*argv, argv)<0){
+		printf("Command not found\n");	
+		exit(1);
+		}
+		else
+		{printf("hi");}
+	}
+
+	else{
+		while (wait(&status) != pid);
+		//printf("Command not found\n");	
+	}
+}
+
+void foo_cd(char *d)
+{
+	//char cwd[256];
+	//char* currentD = getcwd(cwd, sizeof(cwd));
+	//printf("%s\n", d);
+	//printf("hi\n");
+	//char *src = '/';
+	//char *strcat(src, d);  
+	chdir(d);
+
+}
+
+
+
+int help(){
+	printf("HERE IS HELP MANUAL\n");
+	printf("Builtin command:help -->displays help guide\n");
+	printf("Builtin command:exit -->exits mini shell\n");
+	printf("Builtin command:guessgame -->starts guessing game\n");
+	printf("\tTry to select the correct number between 1-100\n");
+	printf("Builtin command: cd -->change directories\n");
+	return 0;	
+}
+
+void guessgame(){
+	printf("Welcome to Guessing Game\n");
+	int random_num = 0;
+	int guess_num = 0;
+	int counter = 0;
+
+	srand(time(NULL));
+	random_num = rand() % 100 + 1;
+	printf("Guess a number from 0 to 100! ");
+	
+	while(1){
+		counter++;
+		scanf("%d", &guess_num);
+		if (guess_num == random_num)
+		{
+			printf("Congrats, %d is the winning number!\n", random_num);
+			printf("You guess correctly in %d attempts\n", counter);
+			break;
+		}
+		if (guess_num < random_num)
+			printf("Your guess is too low. Guess again. ");
+		if (guess_num > random_num)
+			printf("Your guess is too high. Guess again. ");
+	}
+}
+
+int main(){
+	
+	strcpy(str_exit, "exit");
 	// Install our signal handler
 	signal(SIGINT, sigint_handler);
 
-	printf("You can only terminate by pressing Ctrl+C\n");
+	//printf("You can only terminate by pressing Ctrl+C\n");
+	char line[MAX_BUF_SZ];
+	char* argv[100];
+	char* path= "/bin/";
+	char progpath[20];
+	int argc; 
 
 	while(1){
-		printf("Running forever!\n");
-		sleep(1);
-	}
+		printf("mini-shell->");
+		//fgets(commandLine, MAX_BUF_SZ, stdin);
+		//printf("%s\n", commandLine);
+		//printf("%s\n", str_exit);
+		//printf("%d\n", strcmp(commandLine, str_exit));
+		//if (strcmp(commandLine,str_exit) == 10){ exit(0);}
+		//execvp(argv[0], argv);
 
-	return 0;
+		/*
+		char *token;
+		token = strtok(commandLine, " ");
+		int i = 0;
+		while (token != NULL){
+			argv[i] = token;
+			//token = strtok(NULL, " ");
+			i++;
+		}
+		argv[i] = NULL;
+		argc = i;
+		
+		for(i = 0; i <argc; i++){
+			printf("%s\n", argv[0]);
+		}
+		*/
+		//fgets(line, MAX_BUF_SZ, stdin);
+		//printf("\n");
+		gets(line);
+		if (*line != '\0'){
+		parse(line, argv);
+		if (strcmp(argv[0],"exit") == 0){ exit(0);}
+		else if (strcmp(argv[0],"help") == 0){ help();}
+		else if (strcmp(argv[0],"guessgame") == 0){ guessgame();}	
+		else if (strcmp(argv[0],"cd") == 0){ foo_cd(argv[1]);}
+		//else{printf("Command not found\n");}	
+		execute(argv);
+		}
+		}
+
+	return 0;	
 }
 
 
