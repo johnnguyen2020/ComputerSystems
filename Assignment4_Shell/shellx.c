77a82c0783a7d616184df8f26e06e299cab30530
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-17 18:47:59 -0400

    upload shellx

--- Assignment4_Shell/shellx.c
+++ Assignment4_Shell/shellx.c
@@ -6,75 +6,54 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <ctype.h>
 
 
-const int MAX_BUF_SZ = 80;
-char str_exit[4];
-
-// Create a signal handler
+const int MAX_BUF_SZ = 80; //Assumed max size for input buffer
+char str_exit[4]; //size of string exit
+//Create a signal handler
 void sigint_handler(int sig){
-	// Ask yourself why 35?
-	write(1,"mini-shell terminatd\n",21); 
+	write(1,"mini-shell terminatd\n",21); //execute exit syscall
 	exit(0);
 }
 
-
 void parse(char *line, char ** argv){
-	/*
-	while (*line != '\0'){
-		while (*line == ' ' || *line == '\t' || *line == '\n')
-			*line++ = '\0';
-			*argv++ = line;
-		while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
-			line++;
-	}
-	*argv = '\0';
-	*/
 	int i = 0;
-	//char * argv[80];
-	char * token;
-	token = strtok(line," \t\r\n\a");
-	while (token != NULL){
-		argv[i] = token;
+	char * token;  //char pointer of tokens   
+	token = strtok(line," \t\r\n\a"); //line is split by follwing specifiers
+	while (token != NULL){           //loop until null chacter and tokenize items in line into argv array
+		argv[i] = token;        
 		i++;
 		token = strtok(NULL, " \t\r\n\a");
 	}
-	argv[i] = '\0';
+	argv[i] = '\0'; //assign last element null character
+	free(token);
 }
 
 void execute(char **argv){
-	pid_t pid;
-	int status;
+	pid_t pid;         //initialize process id
+	int status;        //varible holding process status
 
-	if((pid = fork()) < 0){
+	if((pid = fork()) < 0){                  //if pid doesn't exist
 		printf("Command not found\n");	
 		exit(1);
 	}
-	
-	else if(pid == 0){
-		if (execvp(*argv, argv)<0){
-		printf("Command not found\n");	
+	else if(pid == 0){                       //if child process
+		if (execvp(*argv, argv)<0){      //execute commands in tokenize line
+		printf("Command not found\n");	 
 		exit(1);
 		}
-		else
-		{printf("hi");}
 	}
-
-	else{
-		while (wait(&status) != pid);
-	}		
+	else{while (wait(&status) != pid);}	//wait for child process to finish executing	
 }
 
-void foo_cd(char *d)
+void foo_cd(char *d)                            //Built in command change directory
 {
-	if (chdir(d) != 0){
-		perror("error");
-	} 
+	if (chdir(d) != 0)
+		{perror("error");} 
 }
 
-
-
-int help(){
+int help(){                                                     //prints help manual
 	printf("HERE IS HELP MANUAL\n");
 	printf("Builtin command:help -->displays help guide\n");
 	printf("Builtin command:exit -->exits mini shell\n");
@@ -85,176 +64,146 @@ int help(){
 }
 
 void guessgame(){
-	printf("Welcome to Guessing Game\n");
-	int random_num = 0;
+	printf("Welcome to Guessing Game\n");            //built in to play guess game
+	int random_num = 0; //initialize variables used in game                         
 	int guess_num = 0;
 	int counter = 0;
-
 	srand(time(NULL));
-	random_num = rand() % 100 + 1;
-	printf("Guess a number from 0 to 100! ");
+	random_num = rand() % 100 + 1;  //call rand library for int from 0 to 100  
+	printf("Guess a number from 1 to 100! ");
 	
-	while(1){
-		counter++;
-		scanf("%d", &guess_num);
-		if (guess_num == random_num)
+	while(1){ //loop until correct number guesses
+		counter++; //track guesses
+		scanf("%d", &guess_num); //take user input
+		if (guess_num == random_num) //if correct nuber guessed, exit loop and function ends
 		{
 			printf("Congrats, %d is the winning number!\n", random_num);
 			printf("You guess correctly in %d attempts\n", counter);
 			break;
 		}
 		if (guess_num < random_num)
-			printf("Your guess is too low. Guess again. ");
+			printf("Your guess is too low. Guess again. ");  //hints for user, continues in loop after execution
 		if (guess_num > random_num)
 			printf("Your guess is too high. Guess again. ");
 	}
 }
 
 int main(){
-	
-	strcpy(str_exit, "exit");
+	strcpy(str_exit, "exit");           //fill buffer with "exit"
 	// Install our signal handler
 	signal(SIGINT, sigint_handler);
-
-	//printf("You can only terminate by pressing Ctrl+C\n");
-	char line[MAX_BUF_SZ];
-	
-	char* argv[80];
-	
-	char* path= "/bin/";
-	char progpath[20];
-	int argc; 
+	char line[MAX_BUF_SZ];	            //initialize with max size
+	char* argv[80];			    //initialize char arrary pointer 
 
 	while(1){
-		printf("mini-shell->");
-		//fgets(commandLine, MAX_BUF_SZ, stdin);
-		//printf("%s\n", commandLine);
-		//printf("%s\n", str_exit);
-		//printf("%d\n", strcmp(commandLine, str_exit));
-		//if (strcmp(commandLine,str_exit) == 10){ exit(0);}
-		//execvp(argv[0], argv);
-
-		/*
-		char *token;
-		token = strtok(commandLine, " ");
-		int i = 0;
-		while (token != NULL){
-			argv[i] = token;
-			//token = strtok(NULL, " ");
-			i++;
+		printf("mini-shell->");          //shell prompt
+		fgets(line, MAX_BUF_SZ, stdin);  //read in command line with max buffer restraint
+		if (isspace(line[0])){           //logic to detect whitespace and pass in while loop
+			;
 		}
-		argv[i] = NULL;
-		argc = i;
 		
-		for(i = 0; i <argc; i++){
-			printf("%s\n", argv[0]);
-		}
-		*/
-		//fgets(line, MAX_BUF_SZ, stdin);
-		//printf("%s\n", line);
-		//printf("\n");
-		gets(line);
-		//printf("before parse");
-		if (*line != '\0' && *line != ' ' && *line != '\t'){
-		//if(line != NULL){
-		parse(line, argv);
-		//printf("finish parse");
-		//print for loop
-	        /*
-		int i;	
-		for(i=0; argv[i] != '\0'; i++){
-			printf("%s\n", argv[i]);
-			//printf("hi\n");
-		}
-		*/
-		int j;
-		int k;
-		char *argvLeft[80];
-		//argvLeft = (char*)malloc(MAX_BUF_SZ*sizeof(char));
-		char *argvRight[80];
-		//argvRight = (char*)malloc(MAX_BUF_SZ*sizeof(char));
-		for(j=0; argv[j] != '\0'; j++){
-			if(*argv[j] != 124){
-				argvLeft[j] = argv[j];
-			
+		else if (line[1] != '\0'){  //execute if line is not empty
+		parse(line, argv); //tokenize command line
+		
+		int i,j,k; //counter variables
+	
+		char *argvLeft[80]; //declare left char array before pipe
+		char *argvRight[80]; //declare right char array after pipe
+		for(j=0; argv[j] != '\0'; j++){    //124 is ascii representation of pipe
+			if(*argv[j] != 124){       //iterate through command until pipe
+				argvLeft[j] = argv[j];   //store before pipe into argvLeft		
 			}
 			else{
 				j++;
-				for(k=0; argv[j] != '\0'; k++){
+				for(k=0; argv[j] != '\0'; k++){  //store after pipe into argvRight
 					argvRight[k] = argv[j];
-					//printf("pipe detect\n");
 					j++;
 				}
 				break;
 			}
 		}
 		
-	int i;	
+		/*                                       //print tokens 	
 		for(i=0; argvLeft[i] != '\0'; i++){
 			printf("%s\n", argvLeft[i]);
-			//printf("hi\n");
 		}	
 		for(i=0; argvRight[i] != '\0'; i++){
 			printf("%s\n", argvRight[i]);
-			//printf("hi\n");
 		}
-
-		if(argvRight[0] != NULL){
-			pid_t pid2, pid3;
-			int mypipe[2];
+		*/
+		if(argvRight[0] != NULL){ //if pipe exists then that means 
+			pid_t pid2, pid3; //create process id 
+			int mypipe[2];    //file desriptor for mypipe
 			int status;
-			pipe(mypipe);
-			pid2 = fork();
+			pipe(mypipe);     //create a pipe    
+			pid2 = fork();    //spawn a new process
 
-			if (pid2==0){
-				dup2(mypipe[0], 0);
-				close(mypipe[1]);
-				execvp(argvRight[0], argvRight);
-				exit(1);
+			if (pid2==0){     //if pid2 is child
+				dup2(mypipe[0], 0); //open write end of pipe and close read end
+				close(mypipe[1]);   // close pipe
+				execvp(argvRight[0], argvRight); //execute command for right of pipe
+				exit(1);  //exit child
 			}
-			else{
-				pid3 = fork();
-				if (pid3 == 0){
-				dup2(mypipe[1], 1);
+			pid3 = fork();   //create second process and execute command for left of pipr
+			if (pid3 == 0){
+				dup2(mypipe[1], 1); 
 				close(mypipe[0]);
 				execvp(argvLeft[0], argvLeft);
 				}
-				else{
-					while (wait(&status) != pid3);		
-					//while (wait(&status) != pid2);		
-				}
-				
-			}
-			int i = 0;
-			while(argvRight[i] != '\0'){
+	
+			close(mypipe[0]); //close both ends of pipe
+			close(mypipe[1]);
+			waitpid(pid2); //wait for processs to finish and then exit
+			waitpid(pid3);
+			
+			i = 0;                           //clear argvLeft
+			while(argvLeft[i] != '\0'){
 				argvLeft[i] = '\0';
 				i++;
 			}
-			int j = 0;
+			j = 0;                           //clear argvRight
 			while(argvRight[j] != '\0'){
 				argvRight[j] = '\0';
 				j++;
 			}
 		}		
 		else{
-			if (strcmp(argv[0],"exit") == 0){ exit(0);}
-			else if (strcmp(argv[0],"help") == 0){ help();}
-			else if (strcmp(argv[0],"guessgame") == 0){ guessgame();}	
-			else if (strcmp(argv[0],"cd") == 0){ foo_cd(argv[1]);}
-			else{execute(argv);}
-			int i = 0;
+			if (strcmp(argv[0],"exit") == 0){ exit(0);} //if command is exit, exit()
+			else if (strcmp(argv[0],"help") == 0){ help();} //triggers help menu
+			else if (strcmp(argv[0],"guessgame") == 0){ guessgame();} //triggers guessgame	 
+			else if (strcmp(argv[0],"cd") == 0){ foo_cd(argv[1]);} //triggers built in cd 
+			else{execute(argv);} //execute command 
+			i = 0;                         //clear argvLeft and argvRight
 			while(argvLeft[i] != '\0'){
 				argvLeft[i] = '\0';
 				i++;
 			}
-			int j = 0;
+			j = 0;
 			while(argvRight[j] != '\0'){
 				argvRight[j] = '\0';
 				j++;
 			}
-		}		
-		}
-	}
+		}	
+	
+			i = 0;
+			while(argvLeft[i] != '\0'){
+				argvLeft[i] = '\0';
+				i++;
+			}
+			j = 0;
+			while(argvRight[j] != '\0'){
+				argvRight[j] = '\0';
+				j++;
+			}
+			k = 0;
+			while(argv[k] != '\0'){
+				argv[k] = '\0';
+				k++;
+			}
 
+		}
+	}	
+	free(argv);
+	free(line);
 	return 0;	
 }
