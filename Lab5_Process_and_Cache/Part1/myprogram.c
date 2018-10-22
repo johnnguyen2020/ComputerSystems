42143e15a55ba257b45df9fa07704a6732e00312
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-22 10:55:38 -0400

    upload lab5

--- Lab5_Process_and_Cache/Part1/myprogram.c
+++ Lab5_Process_and_Cache/Part1/myprogram.c
@@ -1,1 +1,54 @@
-// Take a look at some of the previous examples to create your own program!
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+
+	
+	char* myargv[16];      //buffer size of myargv
+	myargv[0]="/bin/ps";   //first command "ps:
+	//myargv[1]="-F";
+	myargv[2]=NULL; // Terminate the argument list
+	
+	if(fork()==0){
+		execve(myargv[0],myargv,NULL);
+		printf("Child: Should never get here\n");
+		exit(1);
+		}else{	 										
+			wait(NULL); //handy synchronization function again!
+		        //printf("This always prints last\n");
+			myargv[0]="/bin/pwd"; //second command "pwd"
+			myargv[2]=NULL; // Terminate the argument list
+			
+			if(fork()==0){
+			execve(myargv[0],myargv,NULL);
+			printf("Child: Should never get here\n");
+			exit(1);
+			}else{	 										
+				wait(NULL); //handy synchronization function again!
+		   	    	printf("This always prints last\n");
+			    	myargv[0] = "/bin/man"; //third command "man" all in nested for loop ran in children
+					if(fork()==0){
+						execve(myargv[0],myargv,NULL);
+						printf("Child: Should never get here\n");
+					exit(1);	
+					}else{	 										
+						wait(NULL);
+					}	
+			}
+		}	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
