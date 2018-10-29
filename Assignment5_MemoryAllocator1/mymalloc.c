1764b8fc1b1b6817b6413186785d82a95dbad73a
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-10-29 02:41:18 -0400

    upload A5

--- Assignment5_MemoryAllocator1/mymalloc.c
+++ Assignment5_MemoryAllocator1/mymalloc.c
@@ -1,24 +1,24 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 
-struct block_meta{
+struct block{
     size_t size;
-    struct block_meta *next;
+    struct block *next;
     int free;
-    //int magic;
 };
 
-typedef struct block_meta block_meta_t; //defined variable type
-#define META_SIZE sizeof(block_meta_t)  //define size of meta struct
+typedef struct block block_t; //defined variable type
+#define META_SIZE sizeof(block_t)  //define size of meta struct
 
 void *global_base = NULL;               //head for linked list
 
 /*
 //function finds available free space, the allocate space if not possible
 //returns pointer to current start of region on heap
-block_meta_t *find_free_block(block_meta_t **last, size_t size) {
-    block_meta_t *current = global_base;
+block_t *find_free_block(block_t **last, size_t size) {
+    block_t *current = global_base;
     while (current && !(current->free && current->size >= size)) {
         *last = current;
         current = current->next;
@@ -27,27 +27,27 @@ block_meta_t *find_free_block(block_meta_t **last, size_t size) {
 }
 */
 
+//function finds best fit memory space meaning the memory allocated yields the 
+//fewest remaining bits, function takes in block type and size of element
+block_t *find_free_block(block_t **block, size_t size){
+    block_t *currentBlock = global_base; //initialize current pointer
+    block_t *tempBlock = NULL;           //initialize temp pointer
 
-block_meta_t *find_free_block(block_meta_t **block, size_t size){
-    block_meta_t *currentBlock = global_base;
-    block_meta_t *tempBlock = NULL;
-
-    while (currentBlock) {
-        if (currentBlock->free && currentBlock->size >= size){
-            if (tempBlock == NULL){
+    while (currentBlock) {  //while currentBlock until end of segment
+        if (currentBlock->free && currentBlock->size >= size){ //if free and size can fit
+            if (tempBlock == NULL){ //initial set fill tempBlock with currentBlock
                 tempBlock = currentBlock;
             }
-            else if (currentBlock->size < tempBlock->size){                                                                
-                // *block = currentBlock;
-                tempBlock = currentBlock;                                                        
+            else if (currentBlock->size < tempBlock->size){ //logic to reassign tempBlock if                                                                
+                tempBlock = currentBlock;                   //there is a better fit                                     
             }                     
         }    
-        *block = currentBlock;
+        *block = currentBlock;  //continue loop to end of segment             
         currentBlock = currentBlock->next;
     }  
 
-    if (tempBlock == NULL){
-        return currentBlock;
+    if (tempBlock == NULL){   //logic to return best fit for mymalloc
+        return currentBlock; 
     }
     else{
         return tempBlock; 
@@ -55,8 +55,8 @@ block_meta_t *find_free_block(block_meta_t **block, size_t size){
 }
 
 //function requests space from system using sbrk and appends new space to end of linked list
-block_meta_t *request_space(block_meta_t* last, size_t size) {
-    block_meta_t *block;
+block_t *request_space(block_t* last, size_t size) {
+    block_t *block;
     block = sbrk(0); //block = pointer to cuurent data segment break addresss
     void *request = sbrk(size + META_SIZE); //allocates space for meta information
     //assert((void*)block == request); // Not thread safe.
@@ -70,14 +70,13 @@ block_meta_t *request_space(block_meta_t* last, size_t size) {
     block->size = size;
     block->next = NULL;
     block->free = 0;
-    //block->magic = 0x12345678;
     return block;
 }
 
 //mymalloc uses helper functions to check and request space
 void *mymalloc(size_t size) {
       //printf("%zu \n", size);
-      block_meta_t *block; 
+      block_t *block; 
       if (size <= 0) {
               return NULL;                
       }
@@ -90,7 +89,7 @@ void *mymalloc(size_t size) {
         global_base = block;
         } 
     else { //If base pointer is not NULL, check to re-use space
-        block_meta_t *last = global_base;
+        block_t *last = global_base;
         block = find_free_block(&last, size);
         if (!block) { // Failed to find free block.
             block = request_space(last, size); //Request new space
@@ -101,17 +100,16 @@ void *mymalloc(size_t size) {
         else {      // Found free block
             // TODO: consider splitting block here.
             block->free = 0;
-            //block->magic = 0x77777777;
         }           
     }
-    //printf("%p \n", block);
+    printf("%p \n", block);
     return(block+1); //Return pointer to region after meta info
 }
 
 
-//data type to return address of block_meta_t ptr
-block_meta_t *get_block_ptr(void *ptr){
-    return (block_meta_t*)ptr -1;
+//data type to return address of block_t ptr
+block_t *get_block_ptr(void *ptr){
+    return (block_t*)ptr -1;
 }
 
 //Free is passed a pointer that is previously returned from my malloc
@@ -119,9 +117,8 @@ void myfree(void * ptr){
     if(!ptr){
         return; //free can be called on NULL but program just returns
     }
-    block_meta_t* block_ptr = get_block_ptr(ptr);
+    block_t* block_ptr = get_block_ptr(ptr);
     block_ptr->free = 1;
-    //block_ptr->magic = 0x55555555;
 }
 
 //takes in number elements to allocate and size of each element
