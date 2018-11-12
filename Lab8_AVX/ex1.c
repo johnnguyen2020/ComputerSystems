f4d835d59376270e6ae64d8e2cbce6e7a9d4bfb1
John Nguyen <johnnguyen@login-students.ccs.neu.edu>
2018-11-11 21:49:15 -0500

    upload fix

--- Lab8_AVX/ex1.c
+++ Lab8_AVX/ex1.c
@@ -27,8 +27,9 @@ int main(){
 	float buffer[8]; // buffer to fold line read, 8 elements for register parallelization
 
 	__m256 reg_1 = _mm256_set_ps(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0);
-	
-	while(!feof(filePointer)){ // read unitl end of file
+
+    int counter = 0;
+	while(!feof(filePointer) && counter != 4){ // read unitl end of file
 		fscanf(filePointer, "%f", &buffer[0]); // store data in buffer by element 
 		fscanf(filePointer, "%f", &buffer[1]);
 		fscanf(filePointer, "%f", &buffer[2]);
@@ -37,7 +38,8 @@ int main(){
 		fscanf(filePointer, "%f", &buffer[5]);
 		fscanf(filePointer, "%f", &buffer[6]);
 		fscanf(filePointer, "%f", &buffer[7]);
-		
+        counter++;
+
 		__m256 reg_2 = _mm256_setr_ps(buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5], buffer[6], buffer[7]); //set register
 		__m256i result = _mm256_add_ps(reg_1, reg_2); //add results in register 
 		print__m256(result); // print results in result register 
